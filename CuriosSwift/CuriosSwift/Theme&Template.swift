f1435f49dc8a1317c969db60a49baace8fc7e515
Emiaostein <cszwdy@gmail.com>
2015-08-07 17:27:48 +0800

    FIX - change to formaltemplateList

--- CuriosSwift/CuriosSwift/Theme&Template.swift
+++ CuriosSwift/CuriosSwift/Theme&Template.swift
@@ -10,7 +10,8 @@ import Foundation
 
 
 // MARK: - Get theme list
-let GET_THEME_LIST = ["template", "themeList"]
+//let GET_THEME_LIST = ["template", "themeList"]
+let GET_THEME_LIST = ["template", "formalThemeList"]
 func GET_THEME_LIST_paras(start: Int, size: Int) -> String {
   let data = [
     "start":start,
@@ -20,7 +21,8 @@ func GET_THEME_LIST_paras(start: Int, size: Int) -> String {
 
 
 // MARK: - get template list
-let GET_TEMPLATE_LIST = ["template","templateList"]
+//let GET_TEMPLATE_LIST = ["template","templateList"]
+let GET_TEMPLATE_LIST = ["template","formalTemplateList"]
 func GET_TEMPLATE_LIST_paras(themeID: String, start: Int, size: Int) -> String {
   let data: [String: AnyObject] = [
     "themeID":themeID,
